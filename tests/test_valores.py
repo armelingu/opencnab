3d425fbd6d281c2e276e38653243222d676fd97e
Gustavo Armelin <armelingu@gmail.com>
2026-05-30 03:40:51 -0300

    feat: adiciona teste adicional para a função valor_cnab

--- tests/test_valores.py
+++ tests/test_valores.py
@@ -3,3 +3,7 @@ from app.nucleo.valores import valor_cnab
 def test_valor_cnab():
     resultado = valor_cnab(150.75)
     assert resultado == "0000000015075"
+
+def test_valor_cnab():
+    resultado = valor_cnab(10)
+    assert resultado == "0000000015075"
