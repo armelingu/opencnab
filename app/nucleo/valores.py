04af0d6d420eaeabd0612e1883b163f6e3bd72bd
Gustavo Armelin <armelingu@gmail.com>
2026-05-30 13:21:06 -0300

    refactor: corrige formatação e renomeia funções de teste para maior clareza

--- app/nucleo/valores.py
+++ app/nucleo/valores.py
@@ -3,7 +3,7 @@ def valor_cnab(valor, tamanho=13):
     valor_decimal = float(valor) #convertemos em decimal
     valor_centavos = valor_decimal * 100 #multiplicamos por 100 para transformar em centavos
     valor_inteiro = int(valor_centavos) #transforma em inteiro
-    valor_texto = str(valor_inteiro)#converte em string (texto)
+    valor_texto = str(valor_inteiro) #converte em string (texto)
     while len(valor_texto) < tamanho:
         valor_texto = "0" + valor_texto
     return valor_texto
--- tests/test_valores.py
+++ tests/test_valores.py
@@ -1,9 +1,14 @@
 from app.nucleo.valores import valor_cnab
 
-def test_valor_cnab():
+def test_valor_cnab_centavos():
     resultado = valor_cnab(150.75)
     assert resultado == "0000000015075"
 
-def test_valor_cnab():
+def test_valor_cnab_inteiro():
     resultado = valor_cnab(10)
-    assert resultado == "0000000015075"
+    assert resultado == "0000000001000"
+
+def test_valor_cnab_zero():
+    resultado = valor_cnab(0)
+    assert resultado == "0000000000000"
+
